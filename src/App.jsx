b93a007111e17fe08e3d6a3e76cb09b3cb5c184a
batmaninrobin <malikmui@vcu.edu>
2022-10-06 10:46:44 -0400

    chart color not working, cant get length of a list returned from promise. do i understand how to use/apply hooks context and the structure of an application?

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Routes, Route, BrowserRouter} from "react-router-dom";
+import {Routes, Route, BrowserRouter, NavLink} from "react-router-dom";
 import { StockOverviewPage } from './pages/StockOverviewPage';
 import { StockDetailPage } from './pages/StockDetailPage';
 import { ContextProvider } from './Context';
@@ -9,12 +9,13 @@ export default function App() {
     <main className="container">
       <ContextProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<StockOverviewPage />} />
-            <Route path="/detail/:symbol" element={<StockDetailPage />} />
-          </Routes>
+          <NavLink to={"/"}>StockOverviewPage</NavLink>
+            <Routes>
+              <Route path="/" element={<StockOverviewPage />} />
+              <Route path="/detail/:symbol" element={<StockDetailPage />} />
+            </Routes>
         </BrowserRouter>
       </ContextProvider>
     </main>
-  )
+  );
 }
